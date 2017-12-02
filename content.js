dfd8c61f43740e08acf388e379fac3aa4f9c1d97
frazierc98 <frazierc@uindy.edu>
2017-12-02 18:08:13 -0500

    anything

--- content.js
+++ content.js
@@ -40,25 +40,6 @@ if(filename.length % 2 == 0){
     $($.parseHTML(data)).appendTo('body')
   })
 
-<<<<<<< HEAD
-=======
-$.get(chrome.extension.getURL('/content.html'), function(data){
-  $($.parseHTML(data)).appendTo('body')
-})
-
-
-function getFileName() {
-//this gets the full url
-var url = document.location.href;
-//this removes the anchor at the end, if there is one
-url = url.substring(0, (url.indexOf("#") == -1) ? url.length : url.indexOf("#"));
-//this removes the query after the file name, if there is one
-url = url.substring(0, (url.indexOf("?") == -1) ? url.length : url.indexOf("?"));
-//this removes everything before the last slash in the path
-url = url.substring(url.lastIndexOf("/") + 1, url.length);
-//return
-return url;
->>>>>>> db7cc24d26917db899d099c9adb6200864daeeb4
 }
 
 
