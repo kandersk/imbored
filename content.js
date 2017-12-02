4eb3eab62aa4c022fcc5cb2b5df2843626679915
kandersk <kandersk@uindy.edu>
2017-12-02 16:26:40 -0500

    Something

--- content.js
+++ content.js
@@ -4,3 +4,32 @@
 // var firstHref = $("a[href^='http']").eq(0).attr("href");
 //
 // var template = require('content.html');
+// var images = document.getElementsByTagName('img');
+
+// $("html").html('<body>Play a game</body>')
+// // $("html").load("content.html")
+//
+// var contentString = " "
+// contentString = '/content.html'.outerHTML;
+// $("html").html(contentString)
+
+// var div = document.createElement('div');
+//
+// div.textContent = 'Injected!';
+// document.body.appendChild(div);
+
+// $( "*" ).load("content.html")
+
+
+// window.open("https://www.formget.com/mailget/" , '_blank');
+// This
+// location.replace(chrome.extension.getURL('/content.html'))
+// $.get(chrome.extension.getURL('/content.html'), function(data){
+//   $(data).appendTo('body')
+// })
+
+var iframe = document.createElement('iframe')
+iframe.src = chrome.extension.getURL('content.html')
+document.body.appendChild(iframe)
+
+alert(chrome.extension.getURL('content.html'))
