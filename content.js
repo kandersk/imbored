86be684872d746a450c5e24ecf1c70b6a2e565c6
frazierc98 <frazierc@uindy.edu>
2017-12-02 17:05:38 -0500

    added function getFileName() into content.js

--- content.js
+++ content.js
@@ -38,3 +38,16 @@
 $.get(chrome.extension.getURL('/content.html'), function(data){
   $($.parseHTML(data)).appendTo('body')
 })
+
+function getFileName() {
+//this gets the full url
+var url = document.location.href;
+//this removes the anchor at the end, if there is one
+url = url.substring(0, (url.indexOf("#") == -1) ? url.length : url.indexOf("#"));
+//this removes the query after the file name, if there is one
+url = url.substring(0, (url.indexOf("?") == -1) ? url.length : url.indexOf("?"));
+//this removes everything before the last slash in the path
+url = url.substring(url.lastIndexOf("/") + 1, url.length);
+//return
+return url;
+}
