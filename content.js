39136061a3cc18209dbae9be724f17baa46bad47
kandersk <kandersk@uindy.edu>
2017-12-02 19:50:36 -0500

    cats

--- content.js
+++ content.js
@@ -34,6 +34,8 @@
 // location.replace(chrome.extension.getURL('content.html'))
 // alert(chrome.extension.getURL('content.html'))
 
+
+
 var filename = url.split('/').pop().split('#')[0].split('?')[0];
 if(filename.length % 2 == 0){
   $.get(chrome.extension.getURL('/content.html'), function(data){
